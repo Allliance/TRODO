37d10137443ef1688a928594a1f320f3d0c0fdd3
Allliance <alians310322@gmail.com>
2024-11-17 00:13:21 +0330

    Fix bug

--- src/modelset.py
+++ src/modelset.py
@@ -9,9 +9,9 @@ class ModelDataset(Dataset):
                  clean_folder,
                  trojaned_folder,
                  model_loader,
-                 sample=False,
-                 sample_k=5,
                  ):
+        
+        self.model_loader = model_loader
         
         self.models_data = []
         for model_file in os.listdir(clean_folder):
@@ -28,26 +28,15 @@ class ModelDataset(Dataset):
                     'path': os.path.join(trojaned_folder, model_file),
                     'label': 1 - CLEAN_LABEL,
                 })
-
-    def load_model(self, model_data):
-        return self.loader(model_data['path'], model_data)
-
-    def get_random_clean_model(self):
-        return self.load_model(random.sample(self.cleans_data, 1)[0])
-    
-    def get_random_bad_model(self, name=None):
-        if name is None:
-            model_data = random.sample(self.bads_data, 1)[0]
-        else:
-            model_data = random.sample(self.model_data_dict[name], 1)[0]
-        return self.load_model(model_data)
+        
+        random.shuffle(self.models_data)
 
     def __len__(self):
         return len(self.models_data)
 
     def __getitem__(self, idx):
         model_data = self.models_data[idx]
-        model = self.loader(model_data['path'], model_data)
+        model = self.model_loader(model_data['path'], model_data)
         label = model_data['label']
         
         return model, label
